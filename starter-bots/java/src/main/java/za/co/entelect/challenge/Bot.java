3c34cf3223607d505bc8853da4d6b620a4ac4273
Fachry Dennis Heraldi <fachrydennisheraldi@gmail.com>
2022-02-10 20:48:44 +0700

    branch untuk denuid

--- starter-bots/java/src/main/java/za/co/entelect/challenge/Bot.java
+++ starter-bots/java/src/main/java/za/co/entelect/challenge/Bot.java
@@ -2,6 +2,7 @@ package za.co.entelect.challenge;
 
 import za.co.entelect.challenge.command.*;
 import za.co.entelect.challenge.entities.*;
+import za.co.entelect.challenge.enums.PowerUps;
 import za.co.entelect.challenge.enums.Terrain;
 
 import java.util.*;
@@ -11,41 +12,48 @@ import static java.lang.Math.max;
 public class Bot {
 
     private static final int maxSpeed = 9;
-    private List<Integer> directionList = new ArrayList<>();
+    private List<Command> directionList = new ArrayList<>();
 
-    private Random random;
-    private GameState gameState;
-    private Car opponent;
-    private Car myCar;
+    private final static Command ACCELERATE = new AccelerateCommand();
+    private final static Command BOOST = new BoostCommand();
+    private final static Command TURN_RIGHT = new ChangeLaneCommand(1);
+    private final static Command TURN_LEFT = new ChangeLaneCommand(-1);
+    private final static Command DECELERATE = new DecelerateCommand();
+    private final static Command DO_NOTHING = new DoNothingCommand();
+    private final static Command EMP = new EmpCommand();
     private final static Command FIX = new FixCommand();
-
-    public Bot(Random random, GameState gameState) {
-        this.random = random;
-        this.gameState = gameState;
-        this.myCar = gameState.player;
-        this.opponent = gameState.opponent;
-
-        directionList.add(-1);
-        directionList.add(1);
+    private final static Command LIZARD = new LizardCommand();
+    private final static Command OIL = new OilCommand();
+    private final static Command TWEET = new TweetCommand(0, 0);
+    
+    public Bot() {
+        directionList.add(TURN_LEFT);
+        directionList.add(TURN_RIGHT);
     }
 
-    public Command run() {
-        List<Object> blocks = getBlocksInFront(myCar.position.lane, myCar.position.block);
+    public Command run(GameState gameState) {
+        Car myCar = gameState.player;
+        Car opponent = gameState.opponent;
+        
+        List<Object> blocks = getBlocksInFront(myCar.position.lane, myCar.position.block, gameState);
+        
         if (myCar.damage >= 5) {
-            return new FixCommand();
+            return FIX;
         }
+
+
         if (blocks.contains(Terrain.MUD)) {
-            int i = random.nextInt(directionList.size());
-            return new ChangeLaneCommand(directionList.get(i));
+            
         }
-        return new AccelerateCommand();
+
+        return ACCELERATE;
     }
 
     /**
      * Returns map of blocks and the objects in the for the current lanes, returns the amount of blocks that can be
      * traversed at max speed.
      **/
-    private List<Object> getBlocksInFront(int lane, int block) {
+    private List<Object> getBlocksInFront(int lane, int block, GameState gameState) {
         List<Lane[]> map = gameState.lanes;
         List<Object> blocks = new ArrayList<>();
         int startBlock = map.get(0)[0].position.block;
--- starter-bots/java/src/main/java/za/co/entelect/challenge/Main.java
+++ starter-bots/java/src/main/java/za/co/entelect/challenge/Main.java
@@ -6,7 +6,6 @@ import za.co.entelect.challenge.entities.GameState;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -23,7 +22,7 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
         Gson gson = new Gson();
-        Random random = new Random(System.nanoTime());
+        Bot bot = new Bot();
 
         while(true) {
             try {
@@ -33,7 +32,7 @@ public class Main {
                 String state = new String(Files.readAllBytes(Paths.get(statePath)));
 
                 GameState gameState = gson.fromJson(state, GameState.class);
-                Command command = new Bot(random, gameState).run();
+                Command command = bot.run(gameState);
 
                 System.out.println(String.format("C;%d;%s", roundNumber, command.render()));
             } catch (Exception e) {
